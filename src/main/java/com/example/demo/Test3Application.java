9f667917ca04b383efd426524a963850bbe1d008
qurongyao <qurongyao@wison.com>
2020-04-20 11:41:48 +0800

    Revert "启动类 添加 789"
    This reverts commit d786ae34

--- src/main/java/com/example/demo/Test3Application.java
+++ src/main/java/com/example/demo/Test3Application.java
@@ -16,7 +16,6 @@ public class Test3Application {
 		SpringApplication.run(Test3Application.class, args);
 		System.out.println("123");
         System.out.println("456");
-		System.out.println("789");
 	}
 
 }
